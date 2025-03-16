36a55cc2c911f6607416e93444a4c8e071ea766e
dhameliyahit <heetdhameliya59@gmail.com>
2025-03-16 18:51:34 +0530

    otp add

--- controllers/lib/Telegram.js
+++ controllers/lib/Telegram.js
@@ -7,6 +7,10 @@ function sendMessage(messageObj,messageText){
     })
 }
 
+
+const OTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+
 function handleMessage(messageObj){
     if (!messageObj || !messageObj.text) {
         return "Invalid message or message text is empty.";
@@ -16,11 +20,12 @@ function handleMessage(messageObj){
 
     if(messageText.charAt(0) === '/'){
         const command = messageText.substr(1);
-
+        let otp = OTP()
         switch(command){
             case 'hi' : return sendMessage(messageObj,"/hi -> for All command \n /start -> introduction \n /edu -> eduction");
             case 'start': return sendMessage(messageObj,"Hi, Hit Dhameliya This side \n for more info text => /hi ");
             case 'edu': return sendMessage(messageObj,"BCA In VNSGU")
+            case 'otp': return sendMessage(messageObj,otp)
 
             default:
                 return sendMessage(messageObj,"Hey hi, I don't know what you say")
